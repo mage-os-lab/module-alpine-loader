d802ee6dae0b2b6fcfd733bee7572c564874c582
Jisse Reitsma <jisse@yireo.com>
2025-11-10 14:43:28 +0100

    Do not load duplicate AlpineJS under Hyva

---
+++ Util/CurrentTheme.php
@@ -0,0 +1,42 @@
+<?php declare(strict_types=1);
+
+namespace MageOS\AlpineJS\Util;
+
+use Magento\Framework\View\DesignInterface;
+use Magento\Framework\View\Element\Block\ArgumentInterface;
+
+class CurrentTheme implements ArgumentInterface
+{
+    public function __construct(
+        private readonly DesignInterface $design,
+    ) {
+    }
+
+    public function isHyva(): bool
+    {
+        return $this->isTheme('Hyva/default') || $this->isTheme('Hyva/default-csp');
+    }
+
+    public function isLuma(): bool
+    {
+        return $this->isTheme('Magento/blank');
+    }
+
+    public function isBreeze(): bool
+    {
+        return $this->isTheme('Swissup/breeze-blank');
+    }
+
+    private function isTheme(string $themeName): bool
+    {
+        $inheritedThemes = $this->design->getDesignTheme()->getInheritedThemes();
+
+        foreach ($inheritedThemes as $inheritedTheme) {
+            if ($inheritedTheme->getCode() === $themeName) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+}
